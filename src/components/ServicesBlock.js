141b3a43c057a0c6feb3329d00874e2936e9dfaa
srgmkv <ms5533@yandex.ru>
2019-06-30 17:32:34 +0700

    color again

--- src/components/ServicesBlock.js
+++ src/components/ServicesBlock.js
@@ -8,7 +8,6 @@ const mapStateToProps = state => {
 		filterConditions: state.filterConditions,
 		filteredServicesList: state.filteredServicesList
 	}
-
 };
 
 function mapDispatchToProps(dispatch) {
@@ -43,7 +42,9 @@ class ServicesBlock extends React.Component {
 
 				<div className="services-block">
 					{this.props.filteredServicesList.map(el => (
-						<div className="service-item" key={Math.round(Math.random() * 1e8)}>
+						
+						<div className={`service-item ${el.type.replace(' ', '')}`}
+						key={Math.round(Math.random() * 1e8)}>
 							<p>{el.type}</p>
 							<p>{el.price} &#x20bd;</p>
 							<p>{el.company.name}</p>
