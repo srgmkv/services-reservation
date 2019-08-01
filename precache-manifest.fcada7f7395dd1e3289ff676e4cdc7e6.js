6e75101b7177571fb7ed7c586e70104fcfcc4348
srgmkv <ms5533@yandex.ru>
2019-08-01 13:58:31 +0700

    Updates

---
+++ precache-manifest.fcada7f7395dd1e3289ff676e4cdc7e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094707f83a0dfafaf67e2fe414badc81",
+    "url": "/services-reservation/index.html"
+  },
+  {
+    "revision": "1be9a30914c3abf11862",
+    "url": "/services-reservation/static/css/main.14972545.chunk.css"
+  },
+  {
+    "revision": "b1e4a3378f165b0ec88d",
+    "url": "/services-reservation/static/js/2.cfc19663.chunk.js"
+  },
+  {
+    "revision": "1be9a30914c3abf11862",
+    "url": "/services-reservation/static/js/main.43c5b1c0.chunk.js"
+  },
+  {
+    "revision": "265e6fef0091d58b5708",
+    "url": "/services-reservation/static/js/runtime~main.b0a4a83d.js"
+  }
+]);
